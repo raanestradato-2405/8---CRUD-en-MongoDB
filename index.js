383d2c75db7dfb1f6c6a9782941379f3e4e2ff6a
Ramon Antonio Estrada Torres <raanestradato@ittepic.edu.mx>
2021-05-05 10:16:18 -0500

    CRUD MongoDB - Funciones Ordenadas

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var mongoose = require ('mongoose');
+var mongoose = require('mongoose');
 var schema = require('./schema');
 
 mongoose.connect('mongodb://localhost:27017/test');
 
 let Publicacion = mongoose.model('Facebook', schema, 'publicaciones');
 
-let publicacion = new Publicacion ({
+let publicacion = new Publicacion({
     title: 'Feliz Lunes',
     author: 'Ramon Antonio estrada Torres',
     body: 'Imagen',
-    comments:{
+    comments: {
         body: 'Igualmente',
         date: new Date()
-    } ,
+    },
     date: new Date(),
     hidden: 1,
     meta: {
@@ -21,11 +21,87 @@ let publicacion = new Publicacion ({
     }
 });
 
-publicacion.save( function(error){
-    if(error){
+//----------------------------------------------------Crear insercion MongodB
+publicacion.save(function (error) {
+    if (error) {
         console.log(error);
         process.exit(1);
     }
     console.log('Saved!!');
-    process.exit(0)
+    //----------------------------------------------------Busqueda General
+
+    Publicacion.find(function (error, docs) {
+        if (error) {
+            console.log(error);
+            process.exit(1);
+        }
+        console.log('<-------------------Consulta General----------------->');
+        console.log(docs);
+
+        //----------------------------------------------------Busqueda por parametro
+        Publicacion.find({ _id: '6090115f79026b05081c0f70' },
+            function (error, docs) {
+                if (error) {
+                    console.log(error);
+                    process.exit(1);
+                }
+                console.log('<---------------------Consulta por parametro--------------------->');
+                console.log(docs);
+
+                //----------------------------------------------------Actualizar
+                Publicacion.update({ _id: '6090115f79026b05081c0f70' },
+                    { $set: { hidden: false } },
+                    function (error, docs) {
+                        if (error) {
+                            console.log(error);
+                            // process.exit(1);
+
+                        }
+
+
+                        console.log('<---------------------Actualizacion--------------------->');
+                        console.log(docs);
+
+                        //----------------------------------------------------Busqueda General
+
+                        Publicacion.find(function (error, docs) {
+                            if (error) {
+                                console.log(error);
+                                process.exit(1);
+                            }
+                            console.log('<-------------------Consulta General Despues de Actualizar----------------->');
+                            console.log(docs);
+
+                            //-------------------------------------------Borrar
+
+                            Publicacion.findByIdAndRemove({ _id: '6090115f79026b05081c0f70' },
+
+                                function (error, docs) {
+                                    if (error) {
+                                        console.log(error);
+                                        // process.exit(1);
+
+                                    }
+                                    console.log('<---------------------Eliminar--------------------->');
+                                    console.log(docs);
+
+                                    //----------------------------------------------------Busqueda General
+
+                                    Publicacion.find(function (error, docs) {
+                                        if (error) {
+                                            console.log(error);
+                                            process.exit(1);
+                                        }
+                                        console.log('<-------------------Consulta General Despues de Borrar----------------->');
+                                        console.log(docs);
+                                    });
+                                });
+                        });
+
+                    });
+
+            });
+
+    });
+
 });
